2d4b2258a07fd2ffd07c1685b9bb4be68397b314
cmthomas000 <cmthomas000@gmail.com>
2015-11-27 22:42:04 -0700

    working on the hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 
 $(document).ready(function () {
     $(".feature-box").hover(function(){
-        $(this)
+        console.log("hover in");
+        $(this).children(".title").fadeOut(150);
+        $(this).children(".description").fadeIn(150);
+
+    }, function(){
+        console.log("hover out");
+        $(this).children(".title").fadeIn(150);
+        $(this).children(".description").fadeOut(150);
     });
 
 });
