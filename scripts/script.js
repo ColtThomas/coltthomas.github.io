4d27224114c934fb6e7ec155550831eb3b66cc57
cmthomas000 <cmthomas000@gmail.com>
2015-11-27 17:04:03 -0700

    restructure the folders

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+
+document.getElementById("box1").onmouseover = function() { alert("Hover!"); };
+document.getElementById("box1").onmouseout  = function() { alert("Out!"); };
