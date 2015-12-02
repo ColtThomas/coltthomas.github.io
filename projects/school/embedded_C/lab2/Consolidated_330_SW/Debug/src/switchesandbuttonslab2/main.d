040e9e723a0b6b33b7786d9db634e19091f239d0
cmthomas000 <cmthomas000@gmail.com>
2015-12-02 15:55:46 -0700

    cleanup

--- projects/school/embedded_C/lab2/Consolidated_330_SW/Debug/src/switchesandbuttonslab2/main.d
+++
@@ -1,7 +0,0 @@
-src/switchesandbuttonslab2/main.d: ../src/switchesandbuttonslab2/main.c \
- ../src/switchesandbuttonslab2/switches.h \
- ../src/switchesandbuttonslab2/buttons.h
-
-../src/switchesandbuttonslab2/switches.h:
-
-../src/switchesandbuttonslab2/buttons.h:
