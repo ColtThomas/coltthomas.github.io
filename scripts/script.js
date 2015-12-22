17029f2dcd85a2225546519a1dbf695f44e76d56
cmthomas000 <cmthomas000@gmail.com>
2015-12-21 17:53:36 -0700

    touching up the image hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
 
 $(document).ready(function () {
     // Hide the initial description box
-    $(".feature-box .title").hide();
+    $(".feature-box .description").hide();
+    $(".feature-box .description-background").hide();
     
     $(".feature-box").hover(function(){
         console.log("hover in");
-        $(this).children(".title").fadeOut(150);
-        $(this).children(".description").fadeIn(150);
+        $(this).children(".title").fadeOut(50);
+        $(this).children(".description").fadeIn(50);
+        $(this).children(".description-background").fadeIn(50);
 
     }, function(){
         console.log("hover out");
-        $(this).children(".title").fadeIn(150);
-        $(this).children(".description").fadeOut(150);
+        $(this).children(".title").fadeIn(50);
+        $(this).children(".description").fadeOut(50);
+        $(this).children(".description-background").fadeOut(50);
     });
 
 });
