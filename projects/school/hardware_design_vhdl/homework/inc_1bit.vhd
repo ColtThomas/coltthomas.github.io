74ccb1b16a7f653897f9e446f5d6f4dd72a5c03a
cmthomas000 <cmthomas000@users.noreply.github.com>
2016-03-15 15:47:27 -0600

    Cleaning the comments

--- projects/school/hardware_design_vhdl/homework/inc_1bit.vhd
+++ projects/school/hardware_design_vhdl/homework/inc_1bit.vhd
@@ -1,33 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    23:20:40 03/14/2016 
--- Design Name: 
--- Module Name:    inc_1bit - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+-- Colt Thomas -- homework 10
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity inc_1bit is
 	port(
@@ -42,6 +15,5 @@ architecture one of inc_1bit is
 begin
 s <= a xor cin;
 cout <= a and cin;
-
 
 end one;
