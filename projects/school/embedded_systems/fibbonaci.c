2654245e7b5ecb317b972dd77b65779de268e2c1
cmthomas000 <cmthomas000@users.noreply.github.com>
2016-09-06 23:28:22 -0600

    Documentation

--- projects/school/embedded_systems/fibbonaci.c
+++ projects/school/embedded_systems/fibbonaci.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
 #include "platform.h"
 
-//#define FIB_SEED_1 0
-//#define FIB_SEED_2 0
+// Fibonacci sequences require two initial values in the series
+#define FIB_SEED_0 0 
+#define FIB_SEED_1 1
+
+#define FIB_ITERATIONS 6 // Fibonacci sequence length
+
 void print(char *str);
-
-
 
 int main()
 {
     init_platform();
 
-
-    // You could probably use the seed values as global
-    int seed_0 = 0;
-    int seed_1 = 1;
+    // Initialize variables used to calculate the fibonacci number sequence
+    int seed_0 = FIB_SEED_0;
+    int seed_1 = FIB_SEED_1;
     int result;
-    int iterations = 6;
-    int i;
+    int iterations = FIB_ITERATIONS;
     xil_printf("Fib Number Sequence: %d, %d ",seed_0,seed_1);
 
-    // Fibonacci algorithm
-    for (i = 0 ; i < iterations; i++) {
+    // Fibonacci algorithm: x(n) = x(n-1) + x(n-2)
+    for (int i = 0 ; i < iterations; i++) {
     	result = seed_0 + seed_1;
     	seed_0 = seed_1;
     	seed_1 = result;
     	xil_printf(", %d",result);
     }
-    xil_printf("\r\n");
-
-//    xil_printf("I present you a fib number: %d\r\n",result);
+    xil_printf("\r\n"); // Outbut is in the format: 0,1,1,2,3,5,....
 
     cleanup_platform();
 
