ad36106dc938a02af21b79695defe520efaa715e
cmthomas000 <cmthomas000@users.noreply.github.com>
2019-03-18 11:48:40 -0600

    code completed (subject to additonal formatting)

--- projects/school/assembly/lab3.asm
+++ projects/school/assembly/lab3.asm
@@ -22,10 +22,11 @@
 # Task 3 - Use the decoding information for the FAT filesystem to convert this hexadecimal value to a string representation of the date for the file table entry. 
 
 # Convert to big-endian
-#$s4 = loadbyte(0x00,0) // load the first byte with zeros
-#$s4 = loadbyte(0x00,0) // load the second byte with zeros
-#$s4 = loadbyte($t0,0) // load the third byte with first byte given by user
-#$s4 = loadbyte($t0,0) // load the fourth byte with second byte given by user
+# $t0 = readhex($t0) // The provided function will convert the input from a string to an integer
+# savebyte($t0,$sp,1) // save the last byte of $t0 to the stack. Note the offset of 1
+# $t0 >> 8	// right shift by a byte
+# savebyte($t0,$sp,1) // save the last byte of $t0 to the stack. The offset of 0 puts the hex into big-endian
+# $s4 = loadword($sp,0) // load the reversed word at the stack pointer address
 
 # Decode the month, day and year
 #$s0 = readhex($s4) // get the integer value of input hex string
@@ -55,22 +56,26 @@
 # $t0: Stores user input Hex value
 # $t1: Temp register for bit masking
 ######################################################################
-	.data              # Data declaration section
-welcome1: .asciiz "\nProgram 3 - Bit Masking"
-welcome2: .asciiz "\ncode produced by Colt Thomas"
-welcome3: .asciiz "\nThis program will take a hexadecimal date from a FAT16 directory and convert to a string representation"
+		.data              # Data declaration section
+welcome1: 	.asciiz 	"\nProgram 3 - Bit Masking"
+welcome2: 	.asciiz 	"\nCode produced by Colt Thomas"
+welcome3: 	.asciiz 	"\nThis program will take a hexadecimal date from a FAT16 directory and convert to a string representation"
+prompt:  	.asciiz 	"\nPlease enter a Hex number: "
+mesg1:		.asciiz 	"\nThe entered value in decimal is: "
+mesg2:  	.asciiz 	"\n\n"
+result1: 	.asciiz 	"\nThe date of the entered value is: "
+blank: 		.asciiz 		" "
+comma: 		.asciiz 		", "
+#			 	 01234567890123456789012345678901234567890123456
+month: 		.asciiz 	"Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec"
+bye: 		.asciiz 	"\nIt is now safe to turn off your computer"
 
-
-prompt:  .asciiz "\nPlease enter a Hex number: "
-mesg1:	.asciiz "\nThe entered value in decimal is: "
-mesg2:  .asciiz "\n\n"
-bye: 	.asciiz "\nIt is now safe to turn off your computer"
-	.text              # Executable code follows
+		.text              # Executable code follows
 main:
 # Task 1 - Print a welcome message that include: your name, a title, and a brief description of the program.
 
-	li $v0, 4          # Syscall to print a string
-	la $a0, welcome1     # We will display the welcome message (x3 strings)
+	li $v0, 4          	# Syscall to print a string
+	la $a0, welcome1     	# We will display the welcome message (x3 strings)
 	syscall
 	li $v0, 4          
 	la $a0, welcome2     
@@ -81,29 +86,86 @@ main:
 	
 # Task 2 - Prompt the user to enter the hexadecimal date from a FAT16 directory entry
 			
-	li $v0, 4          # Syscall to print a string
-	la $a0, prompt     # We will display the prompt
+	li $v0, 4          	# Syscall to print a string
+	la $a0, prompt     	# We will display the prompt
 	syscall
+
+# Task 3 - Use the decoding information for the FAT filesystem to convert this hexadecimal value to a string representation of the date for the file table entry. 
+		
+	jal readhex        	# call the subroutine to read a hex integer
 	
-	jal readhex        # call the subroutine to read a hex integer
-	
-	move $t0, $v0      # Save the result of reading a hex
-	li $v0, 4          # Get ready to label result
+	move $t0, $v0      	# Save the result of reading a hex
+	li $v0, 4          	# Get ready to label result
 	la $a0, mesg1      
         syscall
         li $v0, 1
         move $a0, $t0
         syscall
-	li $v0, 4          # Get ready to label result
+	li $v0, 4          	# Get ready to label result
 	la $a0, mesg2      
         syscall
 
-	li $v0, 4  	# print farewell message       
+# Convert to big-endian
+	sb $t0, 1($sp)		# Save the first byte to the stack pointer offset by 1 byte 	$t0 (0x0000AABB) -> $sp (0x0000BB00)
+	srl $t0, $t0, 8		# Shift right to discard saved byte				$t0 (0x000000AA)
+	sb $t0, 0($sp)		# Save the next byte at the stack pointer 			$t0 (0x000000AA) -> $sp (0x0000BBAA)
+	lw $s4, 0($sp) 		# Load the word stored at the stack pointer 
+
+# Decode the month, day and year
+	andi $s1, $s4, 65024 	# Year bit mask: 0xFE00
+	srl $s1, $s1, 9		# Year shift correction
+	addi $s1, $s1, 1980	# FAT 16 year date increments from 1980, so we add 1980 
+	
+	andi $s2, $s4, 480 	# Month bit mask: 0x01E0
+	srl $s2, $s2, 5		# Month shift correction
+	addi $s2, $s2, -1	# We generate the starting index of zero-based char array to get the correct month
+				# 01234567890 ... 48   -index number
+				# Jan Feb Mar ... D.ec -month
+	sll $s2, $s2, 2		# Multiply by 4 by bit shifting
+
+	andi $s3, $s4,31 	# Day bit mask: 0x001F
+
+	
+# Task 4 - Display the date corresponding to the hexadecimal value as a string	
+	
+	li $v0, 4          	# Begin printing the date result
+	la $a0, result1     
+	syscall
+	
+	li $v0, 11          	# print char syscall
+	la $a1, month		# store our month string to $a1
+	add $a1, $a1, $s2 	# print first char of the month    
+	lbu $a0, ($a1)		# load our first byte into the $a0 register as argument
+	syscall
+	addi $a1, $a1, 1 	# print second char of the month by incrementing our index    
+	lbu $a0, ($a1)
+	syscall
+	addi $a1, $a1, 1 	# print third char of the month by incrementing our   
+	lbu $a0, ($a1)
+	syscall
+	
+	li $v0, 4          	# print " " 
+	la $a0, blank     
+	syscall
+	
+	li $v0, 1          
+	la $a0, ($s3)		# print the day     
+	syscall
+	
+	li $v0, 4          	# print ", "
+	la $a0, comma     
+	syscall
+	
+	li $v0, 1          
+	la $a0, ($s1)		# print the year     
+	syscall
+	
+	li $v0, 4  		# print farewell message       
 	la $a0, bye  
 	syscall
 
-	li    $v0, 10          # terminate program run and
-	syscall                # return control to system
+	li $v0, 10          	# terminate program run and
+	syscall                	# return control to system
 
 
 # END OF MAIN PROGRAM # Subroutines are below #######################
